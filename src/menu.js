b207704191e5bb39d44ce2324bdca82e2610be2e
Christopher Jazz Esguerra <christopher.esguerra2@gmail.com>
2026-06-11 10:32:51 +0800

    Added picture for menu items

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,37 @@
+import burgerImg from "./images/burger.jpg";
+import friesImg from "./images/fries.jpg";
+import pizzaImg from "./images/pizza.jpg";
+import pastaImg from "./images/pasta.jpg";
 import { clearMain } from "./dom";
 import { createElement } from "./elements";
 import { createMenuCard } from "./menuCard";
 
 
 const menuItems = [
-  { name: "Burger", price: 250 },
-  { name: "Fries", price: 100 },
-  { name: "Pizza", price: 450 },
-  { name: "Pasta", price: 300 },
+  {
+    name: "Burger",
+    price: 250,
+    image: burgerImg,
+    description: "Juicy beef burger with cheese",
+  },
+  {
+    name: "Fries",
+    price: 100,
+    image: friesImg,
+    description: "Crispy golden fries",
+  },
+  {
+    name: "Pizza",
+    price: 450,
+    image: pizzaImg,
+    description: "Wood-fired pizza",
+  },
+  {
+    name: "Pasta",
+    price: 300,
+    image: pastaImg,
+    description: "Creamy pasta dish",
+  },
 ];
 
 export function loadMenu() {
--- src/menuCard.js
+++ src/menuCard.js
@@ -4,10 +4,28 @@ export function createMenuCard(item) {
   const card = createElement("div");
   card.classList.add("menu-card");
 
-  const name = createElement("h3", item.name);
-  const price = createElement("p", `₱${item.price}`);
+  const image = document.createElement("img");
+  image.src = item.image;
+  image.alt = item.name;
 
-  card.append(name, price);
+  const name = createElement("h3", item.name);
+
+  const description = createElement(
+    "p",
+    item.description
+  );
+
+  const price = createElement(
+    "p",
+    `₱${item.price}`
+  );
+
+  card.append(
+    image,
+    name,
+    description,
+    price
+  );
 
   return card;
 }
